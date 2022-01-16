92a2424760300cee6f60ae28fde981a8749635e5
Arshad <62069510+Arshadsanto@users.noreply.github.com>
2022-01-16 22:09:50 +0100

    Update ratings.js

--- js/ratings.js
+++ js/ratings.js
@@ -1,24 +1,24 @@
-function collect_ratings(){
+const collect_ratings = () => {
     let ratings = {
-        count: 0,
-        num = 0,
-        average = 0
-    }
+        'count': 0,
+        'num' : 0,
+        'average' : 0
+    };
     let rating = 0;
 
-const elements = document. querySelectorAll('rating');
+const elements = document. querySelectorAll('.rating');
 
 elements.forEach(element=>){
-    rating = prseInt(element.id.replace('star',''));
-    ratings.count = parseInt(element.value);
-    ratings.sum = parseInt(element.value)*rating;
+    rating = parseInt(element.id.replace('star',''));
+    ratings.count += parseInt(element.value);
+    ratings.sum += parseInt(element.value)*rating;
 }); 
 if (ratings.count !=== 0){
     ratings.average = ratings.sum/ratings.count;
     }
     return ratings;
 }
-document.addEventListener('change' (){
+document.addEventListener('change' ()=>{
     const ratings = collect_ratings();
     document.querySelector('#average').value = ratings.average.toFixed(2);
 });
